e5547084d6f993e3e1d68c16a67462ff2ecde89a
Santosh Krishnamurthy <san.b350@gmail.com>
2023-03-09 14:41:26 -0600

    Initial Commit

---
+++ proto-loading-shimmer-animatioon-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  proto-loading-shimmer-animatioon-ios
+//
+//  Created by Santosh Krishnamurthy on 3/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
